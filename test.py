76ebe7e66f16da22b0427c018c70ed434ac51e17
Sve4ka <sve4ka_one@mail.ru>
2021-01-17 22:18:57 +0500

    набросок пулиv2.2 пуляет людьми как бог

--- test.py
+++ test.py
@@ -7,90 +7,157 @@ SIZE_PERS = [55, 80]
 SIZE_CELL = 40
 
 
-class YAwareGroup(pygame.sprite.Group):
-    def by_y(self, spr):
-        return spr.pos.y + spr.dop
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, group, otkuda, kuda, sc, event):
+        super().__init__(group)
+        self.otkuda = otkuda
+        self.kuda = kuda
+        self.velocity = 10
+        self.screen = sc
+        self.fps = 60
+        self.height, self.width = SIZE
+        #  возможно будет двигатся лишь на положительную сторону
+        #  по оси у(тогда надо попробовать или иф или арксинус и теорему пифагора
+        #  куда(?) нужно менять координату для каждого кадра
+        self.alfa = math.atan(
+            (self.kuda[0] - self.otkuda[0]) / (self.kuda[1] - self.otkuda[1]))  # нашли направление вектора(градус)
+        self.moving = [
+            -self.velocity * math.sin(self.alfa) * (self.otkuda[1] - self.kuda[1]) // abs(self.otkuda[1] - self.kuda[1]),
+            -self.velocity * math.cos(self.alfa) * (self.otkuda[1] - self.kuda[1]) // abs(self.otkuda[1] - self.kuda[1])]
+        self.image = mandalorian1
+        self.rect = self.image.get_rect()
+        self.rect.x = otkuda[0]
+        self.rect.y = otkuda[1]
+        # print(self.moving, (self.otkuda[0] - self.kuda[0]), abs(self.otkuda[0] - self.kuda[0]), (self.otkuda[1] - self.kuda[1]), abs(self.otkuda[1] - self.kuda[1]))
+        # print(otkuda, kuda, SIZE, self.alfa, (self.kuda[0] - self.otkuda[0]), (self.kuda[1] - self.otkuda[1]), self.moving)
+        self.update(event)
 
-    def draw(self, surface):
-        sprites = self.sprites()
-        surface_blit = surface.blit
-        for spr in sorted(sprites, key=self.by_y):
-            self.spritedict[spr] = surface_blit(spr.image, spr.rect)
-        self.lostsprites = []
+    def update(self, *args):
+        self.rect = self.rect.move(*self.moving)
+
+    def move_bullet(self):
+        pass
+
+    def isshootedByPlayer(self):
+        pass
 
 
-def render_board():
-    for w in range(SIZE[0] // SIZE_CELL):
-        for h in range(SIZE[1] // SIZE_CELL):
-            BRD(all_sprites, board_sprites, wall_horizon_sprites, wall_vertical_sprites, w, h)
+def move():
+    global wait, pos, stap
+    for i in flags:
+        wait += 1
+        if flags[i]:
+            wait = 0
+            image1 = mandalorian1_move1
+            image2 = mandalorian1_move2
+            check_and_break = False
+            if i == pygame.K_w and board.check_in_stop((pos[0], pos[1] - sp)):
+                if pos[1] - sp >= 1:
+                    screen.fill((0, 0, 0))
+                    board.render_level()
+                    pos = (pos[0], pos[1] - sp)
+                    image1 = mandalorian4_move1
+                    image2 = mandalorian4_move2
+                    check_and_break = True
+            if i == pygame.K_a and board.check_in_stop((pos[0] - sp, pos[1])):
+                if pos[0] - sp >= 1:
+                    screen.fill((0, 0, 0))
+                    board.render_level()
+                    pos = (pos[0] - sp, pos[1])
+                    image1 = mandalorian3_move1
+                    image2 = mandalorian3_move2
+                    check_and_break = True
+            if i == pygame.K_s and board.check_in_stop((pos[0], pos[1] + sp)):
+                if pos[1] < size[1] - size_character[1]:
+                    screen.fill((0, 0, 0))
+                    board.render_level()
+                    pos = (pos[0], pos[1] + sp)
+                    image1 = mandalorian1_move1
+                    image2 = mandalorian1_move2
+                    check_and_break = True
+            if i == pygame.K_d and board.check_in_stop((pos[0] + sp, pos[1])):
+                if pos[0] <= size[0] - size_character[0]:
+                    screen.fill((0, 0, 0))
+                    board.render_level()
+                    pos = (pos[0] + sp, pos[1])
+                    image1 = mandalorian2_move1
+                    image2 = mandalorian2_move2
+                    check_and_break = True
+            if i and check_and_break:
+                stap += 1
+                if stap <= 20:
+                    screen.blit(image1, pos)
+                elif stap <= 40:
+                    screen.blit(image2, pos)
+                else:
+                    screen.blit(image2, pos)
+                    stap = 0
+        if wait >= 50:
+            screen.fill((0, 0, 0))
+            board.render_level()
+            screen.blit(mandalorian1, pos)
+        board.on_line(pos)
 
 
-def render_box():
-    level = 'test_level.txt'
-    txt_level = (open(level, mode='rt').read()).split(';')
-    for i in range(len(txt_level)):
-        txt = txt_level[i].split()
-        Box(all_sprites, box_sprites, txt[0:2])
-        Objects(all_sprites, object_sprites, draw_sprites, txt[0:2], txt[3])
-        boxes.append([int(txt[0]) * 40, int(txt[1]) * 40 + 80])
+def shoot(pos1, pos2):
+    if pos1 != pos2:
+        mimimum = min(pos1[0], pos2[0])
+        coor1 = (pos1[0] - pos2[0]) / mimimum
+        coor2 = (pos1[1] - pos2[1]) / mimimum
+        sped = [coor1, coor2]
+        shoot_coord.append([[pos1[0], pos1[1]], [pos2[0], pos2[1]], sped])
 
 
 if __name__ == '__main__':
     pygame.init()
     all_sprites = pygame.sprite.Group()
-    draw_sprites = YAwareGroup()
-    bullet_sprites = pygame.sprite.Group()
-    box_sprites = pygame.sprite.Group()
-    person_sprites = pygame.sprite.Group()
-    object_sprites = pygame.sprite.Group()
-    board_sprites = pygame.sprite.Group()
-    wall_horizon_sprites = pygame.sprite.Group()
-    wall_vertical_sprites = pygame.sprite.Group()
     shoot_coord = list()
-    size = SIZE
-    boxes = []
-    dt = 0
+    size = (1280, 720)
     screen = pygame.display.set_mode(size)
     screen.fill((0, 0, 0))
-    size_character = SIZE_PERS
+    size_character = (55, 80)
     pos = (size[0] // 2 - size_character[0] // 2, size[1] // 2 - size_character[1] // 2)
+    pygame.display.flip()
     running = True
+    flags = {}
+    stap = 0
+    s = ''
+    wait = 0
+    sp = 2
+    smome = False
     logo = pygame.image.load('files/textures/Logo/logo.png')
-    board = Board(screen, *SIZE)
+    mandalorian1 = pygame.image.load('files/textures/main_charachter_1/mandalorian.png')
+    mandalorian1_move1 = pygame.image.load('files/textures/main_charachter_1/mandalorian_move1.png')
+    mandalorian1_move2 = pygame.image.load('files/textures/main_charachter_1/mandalorian_move2.png')
+    mandalorian1_shot = pygame.image.load('files/textures/main_charachter_1/mandalorian_shot.png')
+
+    mandalorian2_move1 = pygame.image.load('files/textures/main_charachter_1/mandalorian_right_move1.png')
+    mandalorian2_move2 = pygame.image.load('files/textures/main_charachter_1/mandalorian_right_move2.png')
+    mandalorian2_shot = pygame.image.load('files/textures/main_charachter_1/mandalorian_right_shot.png')
+
+    mandalorian3_move1 = pygame.image.load('files/textures/main_charachter_1/mandalorian_left_move1.png')
+    mandalorian3_move2 = pygame.image.load('files/textures/main_charachter_1/mandalorian_left_move2.png')
+    mandalorian3_shot = pygame.image.load('files/textures/main_charachter_1/mandalorian_left_shot.png')
+
+    mandalorian4_move1 = pygame.image.load('files/textures/main_charachter_1/mandalorian_back_move1.png')
+    mandalorian4_move2 = pygame.image.load('files/textures/main_charachter_1/mandalorian_back_move2.png')
+    board = Board(screen, 1280, 720)
     board.render_pole()
     board.lvl('test_level.txt')
     screen.blit(logo, (0, 0))
-    splash = True  # заставка, ждем начала игры
-    while splash:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                splash = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                splash = False
-            if event.type == pygame.KEYDOWN:
-                splash = False
-        all_sprites.draw(screen)
-        pygame.display.flip()
-    render_board()
-    render_box()
-    player = Person(all_sprites, person_sprites, wall_horizon_sprites, wall_vertical_sprites, draw_sprites,
-                    box_sprites, boxes)
-    screen.fill((0, 0, 0))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                bullet = Bullet(all_sprites, bullet_sprites, object_sprites, wall_horizon_sprites,
-                                wall_vertical_sprites, player.rect, event.pos, screen, event)
+                bullet = Bullet(all_sprites, pos, event.pos, screen, event)
             if event.type == pygame.KEYDOWN:
-                player.move(event.key)
+                flags[event.key] = True
+                smome = True
             if event.type == pygame.KEYUP:
-                player.down(event.key)
-        screen.fill((255, 255, 255))
+                flags[event.key] = False
+        move()
         all_sprites.draw(screen)
-        draw_sprites.draw(screen)
-        all_sprites.update(dt)
+        all_sprites.update()
         pygame.display.flip()
     pygame.quit()
