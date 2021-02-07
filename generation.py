d64aebb4533d2486b872a6af7c36a7a954d8b68c
__gyulka__ <romagyuli-zade@yandex.ru>
2021-02-07 18:25:41 +0500

    сделана генерация, путь до конца гарантирован

--- generation.py
+++ generation.py
@@ -1,31 +1,59 @@
-import csv
 import random
 
-import consts
-import Pole
 BOX = 'box'
-RIGHT = '0'
-LEFT = '1'
-UP = '2'
+RIGHT = '2'
+LEFT = '4'
+UP = '1'
 DOWN = '3'
 
 
 def gen_map():
-    a = consts.a + 0
-    b = consts.b + 0
-    lis = [[gen_room() for i in range(b)] for i in range(a)]
+    a = 5
+    b = 5
+    lis = [[None for i in range(b + 1)] for i in range(a + 1)]
     lis[a // 2][0] = gen_start()
-    i = 0
+    i = 1
     j = 2
+    from1 = LEFT
+    ans = ''
+    while i != a or j != 2:
+        if i == b:
+            ans += DOWN if j > 2 else UP
+            lis[j][i] = gen_room(from1, ans[-1])
+            from1 = UP if j > 2 else DOWN
+        elif j == a:
+            ans += RIGHT
+            lis[j][i] = gen_room(from1, ans[-1])
+            from1 = LEFT
+        elif j == 0:
+            ans += RIGHT
+            lis[j][i] = gen_room(from1, ans[-1])
+            from1 = LEFT
+        elif i == b:
+            ans += DOWN if j > 2 else UP
+            lis[j][i] = gen_room(from1, ans[-1])
+            from1 = UP if j > 2 else DOWN
+        else:
+            choises = [k for k in [UP, RIGHT, DOWN] if k != from1]
+            ans += random.choice(choises)
+            lis[j][i] = gen_room(from1, ans[-1])
+            from1 = {RIGHT: LEFT, UP: DOWN, LEFT: RIGHT, DOWN: UP}[ans[-1]]
+
+        if from1 == LEFT:
+            i += 1
+        elif from1 == UP:
+            j -= 1
+        elif from1 == DOWN:
+            j += 1
+    return lis, ans
 
 
-def gen_room():
-    return None
+def gen_room(from1, to1):
+    return f'files/levels/{from1}_{to1}_{random.randint(1, 2)}.txt'
+
 
 def gen_start():
-    with open(f'start_{random.randint(1, 2)}', 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
-        lis = []
-        for i in reader:
-            lis.append(i)
-        return lis
+    return f'files/levels/start/start_{random.randint(1, 2)}.txt'
+
+
+print(gen_map())
